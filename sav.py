45fef1040628dfae4ec0e89cc0f46f60b59b24c6
David Silva <david.arsilva@gmail.com>
2022-01-13 16:05:38 +0000

    main func,  draw func and list func

--- sav.py
+++ sav.py
@@ -1,7 +1,10 @@
 import pygame
 import random
+
+from pygame import draw
 pygame.init()
 
+# general class with basic drawing info
 class DrawInfo:
     BLACK = 0,0,0
     WHITE = 255,255,255
@@ -15,6 +18,7 @@ class DrawInfo:
     X_MARGIN = 80
     Y_MARGIN = 120
 
+    # windows init
     def __init__(self, width, height, list):
         self.width = width
         self.height = height
@@ -23,6 +27,7 @@ class DrawInfo:
         pygame.display.set_caption("Sorting Algorithm Visualizer")
         self.set_list(list)
     
+    # adaptive display size with margins
     def set_list(self,list):
         self.list = list
         self.min_val = min(list)
@@ -32,3 +37,46 @@ class DrawInfo:
         self.bars_max_height = round((self.height - self.Y_MARGIN) / (self.max_val - self.min_val))
         self.start_x = self.X_MARGIN // 2
 
+# basically refills the window every frame
+def draw(draw_info):
+    # the way I have done this isn't the most optimized or efficent way of doing
+    ## but it makes sure it gets rid of overlays, shadows and other artifacts
+    draw_info.window.fill(draw_info.BG_COLOR)
+    pygame.display.update()
+
+def generate_starting_list(n, min_val, max_val):
+
+    list = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        list.append(val)
+
+    return list
+
+def main():
+    run = True 
+    clock = pygame.time.Clock()
+
+    # core editable values
+    n = 50
+    min = 1
+    max = 100
+
+    list = generate_starting_list(n, min, max)
+    draw_info = DrawInfo(800, 600, list)
+
+    while run:
+        clock.tick(60)
+
+        draw(draw_info)
+
+        for event in pygame.event.get():
+            # handling top right quit bottom
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
