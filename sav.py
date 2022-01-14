10927524891f6597cb56ec2f6d88a6c63b69bd5a
David Silva <david.arsilva@gmail.com>
2022-01-14 14:39:07 +0000

    added draw_list which calculates the bars positions and draws them

--- sav.py
+++ sav.py
@@ -1,20 +1,25 @@
 import pygame
 import random
 
-from pygame import draw
 pygame.init()
 
 # general class with basic drawing info
 class DrawInfo:
-    BLACK = 0,0,0
-    WHITE = 255,255,255
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
 
-    BLUE = 0,0,255
-    GREEN = 0,255,0
-    RED = 255,0,0
+    BLUE = 52, 152, 219
+    GREEN = 46, 204, 113
+    RED = 231, 76, 60
 
-    GREY = 128,128,128
     BG_COLOR = WHITE
+
+    GRADIENTS = [
+        (197, 237, 245),
+        (74, 135, 154),
+        (135, 185, 198)
+    ]
+
     X_MARGIN = 80
     Y_MARGIN = 120
 
@@ -23,26 +28,39 @@ class DrawInfo:
         self.width = width
         self.height = height
 
-        self.window = pygame.display.set_mode((width,height))
+        self.window = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Sorting Algorithm Visualizer")
         self.set_list(list)
     
     # adaptive display size with margins
-    def set_list(self,list):
+    def set_list(self, list):
         self.list = list
         self.min_val = min(list)
         self.max_val = max(list)
 
-        self.bars_width = round(self.width - self.X_MARGIN / len(list))
+        self.bars_width = round((self.width - self.X_MARGIN) / len(list))
         self.bars_max_height = round((self.height - self.Y_MARGIN) / (self.max_val - self.min_val))
         self.start_x = self.X_MARGIN // 2
 
 # basically refills the window every frame
 def draw(draw_info):
+
     # the way I have done this isn't the most optimized or efficent way of doing
     ## but it makes sure it gets rid of overlays, shadows and other artifacts
     draw_info.window.fill(draw_info.BG_COLOR)
+    draw_list(draw_info)
     pygame.display.update()
+
+def draw_list(draw_info):
+    list = draw_info.list
+    
+    for i, val in enumerate(list):
+        x = draw_info.start_x + i * draw_info.bars_width
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.bars_max_height
+        color = draw_info.GRADIENTS[i % 3]
+        
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.bars_width , draw_info.height))
+
 
 def generate_starting_list(n, min_val, max_val):
 
@@ -60,16 +78,15 @@ def main():
 
     # core editable values
     n = 50
-    min = 1
-    max = 100
+    min_val = 1
+    max_val = 100
 
-    list = generate_starting_list(n, min, max)
+    list = generate_starting_list(n, min_val, max_val)
     draw_info = DrawInfo(800, 600, list)
+    draw(draw_info)
 
     while run:
         clock.tick(60)
-
-        draw(draw_info)
 
         for event in pygame.event.get():
             # handling top right quit bottom
